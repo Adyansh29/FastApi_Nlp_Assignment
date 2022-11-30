9e96346d9ee118d9bc7a396d92f6656eab04869c
Addy2909 <115083622+Addy2909@users.noreply.github.com>
2022-11-30 10:47:50 +0530

    ner file

--- ner.py
+++ ner.py
@@ -1,8 +1,6 @@
 import spacy
 nlp = spacy.load("en_core_web_md")
 
-# text = "My friend Rohit works in Apple. His age is 26. "
-
 
 def return_entity(text):
     doc = nlp(text)
@@ -10,9 +8,9 @@ def return_entity(text):
 
     if doc.ents:
         for ent in doc.ents:
-            di[ent.text] = ent.label_
+            di[ent.text] = {ent.label_: spacy.explain(ent.label_)}
         return di
     else:
         return {"Message": "Name Entity cannot be recognized"}
 
-# print(return_entity(text))
+
